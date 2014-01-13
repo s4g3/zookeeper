43d9aae5ba5dadbd4ce96dbfa530c80ed2634ecc
s4g3 <sage.harris@hotmail.com>
2014-01-14 10:52:40 +1300

    Revert e6caa68..3b24c8b
    This rolls back to commit e6caa68be178ff0ed2f1083ca42c9569af8d0295.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
-  'animals#index'
+def root(options = {})
+  match '/', { :as => :root, :via => :get }.merge!(options)
+end
 
+Zookeeper::Application.routes.draw do
+  resources :animals
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  
+  # root 'welcome#index'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
