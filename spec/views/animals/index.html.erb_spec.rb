8fb2a86f02171249c90e1cdbd52d6e2da23ecdc1
Daniel Fone <daniel@fone.net.nz>
2014-02-20 14:06:47 +1300

    New index spec

---
+++ spec/views/animals/index.html.erb_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe 'animals/index' do
+  before { assign :animals, [elephant, lion] }
+  before { render }
+  subject { rendered }
+
+  let(:elephant) { stub_model Animal, name: 'Dumbo', species: 'Elephant', weight: 5432, notes: "Flies" }
+  let(:lion)     { stub_model Animal, name: 'Simba', species: 'Lion', weight: 181, notes: "Doesn't fly" }
+
+  it 'should render a table of animals' do
+    expect(rendered).to have_content <<-TABLE
+      Name    Species   Weight      Notes
+      Dumbo   Elephant  5,432 kg    Flies
+      Simba   Lion        181 kg    Doesn't fly
+    TABLE
+  end
+
+  it 'should link to the animal from the name' do
+    expect(rendered).to have_link "Dumbo", href: url_for(elephant)
+  end
+
+  it { should have_link "New Animal", href: new_animal_path }
+
+  context 'with a long note' do
+    before { elephant.notes = "Lorem ipsum " * 30 }
+    before { render }
+
+    it 'should truncate the note' do
+      expect(rendered).to have_content "Lorem ipsum Lorem ipsum Lor..."
+    end
+  end
+
+end
