b91212fb9be72335402d2a4b9b10cd3eeb37c6b0
Daniel Fone <daniel@fone.net.nz>
2014-02-20 14:06:41 +1300

    More comprehensive show spec

--- spec/views/animals/show.html.erb_spec.rb
+++ spec/views/animals/show.html.erb_spec.rb
@@ -3,20 +3,32 @@ require 'spec_helper'
 describe 'animals/show' do
   before { assign :animal, animal }
   before { render }
+  subject { rendered }
 
-  let(:animal) { stub_model Animal, notes: "First line\nSecond line", weight: 5432 }
+  let(:animal) { stub_model Animal, name: 'Dumbo', species: 'Elephant', notes: 'Flies', weight: 5432 }
 
-  it 'should show the use simple format for the notes field' do
-    expect(rendered).to include <<-HTML.strip_heredoc
-      <strong>Notes:</strong>
-      <p>First line
-    <br />Second line</p>
-    HTML
+  it 'should render the animal' do
+    should have_content <<-PAGE
+      Name:     Dumbo
+      Species:  Elephant
+      Weight:   5,432 kg
+      Notes:    Flies
+
+      Back | Edit | Destroy
+    PAGE
   end
 
-  it 'should format the weight' do
-    expect(rendered).to have_content '5,432 kg'
+  it { should have_link "Back", animals_path }
+  it { should have_link "Edit", edit_animal_path(animal) }
+  it { should have_link "Destroy", animal_path(animal) }
+
+  context 'with multiline notes' do
+    before { animal.notes = "First line\nSecond line" }
+    before { render }
+
+    it 'should format the new lines' do
+      expect(rendered).to include "First line\n<br />Second line"
+    end
+
   end
-
-
 end
